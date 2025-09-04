d8274a2ee261e5298087117b88df7aff2d57d480
kanade0811 <tooyamanozomi@gmail.com>
2025-09-04 23:57:37 +0900

    初回コミット

---
+++ script.js
@@ -0,0 +1,8 @@
+const input=document.getElementById("input");
+const guessings=[a];
+
+function a(input){
+    console.log("あなたの入力した数",input,"に3を掛けて、6を足して、3で割って、2を引きます")
+    const output=(input*3+6)/3-2;
+    console.log("あなたの入力した数は、",output,"です")
+}
